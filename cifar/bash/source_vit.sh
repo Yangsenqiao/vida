49b28452fd44db2e468105806b173fb04f787ce7
Yangsenqiao <yangsenqiao2002@163.com>
2024-03-26 20:12:08 +0800

    Fix bug: add args to the bash file

--- cifar/bash/source_vit.sh
+++ cifar/bash/source_vit.sh
@@ -2,4 +2,4 @@ export PYTHONPATH=.
 conda deactivate
 conda activate vida
 
-python cifar10c_vit.py --cfg cfgs/cifar10/source.yaml
+python cifar10c_vit.py --cfg cfgs/cifar10/source.yaml --checkpoint [your-ckpt-path] --data_dir [your-data-dir] 
--- cifar/bash/tent_vit.sh
+++ cifar/bash/tent_vit.sh
@@ -2,4 +2,4 @@ export PYTHONPATH=.
 conda deactivate
 conda activate vida
 
-python cifar10c_vit.py --cfg cfgs/cifar10/tent.yaml
+python cifar10c_vit.py --cfg cfgs/cifar10/tent.yaml --checkpoint [your-ckpt-path] --data_dir [your-data-dir] 
